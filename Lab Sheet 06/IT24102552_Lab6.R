e4d98759a3959c2d2baac4524dfc1866bc2e4f43
IT24102552 <IT24102552@my.sliit.lk>
2025-09-15 11:52:27 +0530

    lab 6

---
+++ Lab Sheet 06/IT24102552_Lab6.R
@@ -0,0 +1,21 @@
+setwd("C:\\Users\\Dhanuka\\Desktop\\IT24102552_lab6")
+getwd
+#Question 01
+#Part 01
+#Random variable X has binomial distribution with n = 50 and p = 0.85
+
+#Part 02
+#Probability of getting an exact value can be calculate using this command
+dbinom(47,50,0.85)
+
+#Question 02
+#Part 01
+#X = number of calls receive by the call center per hour
+
+#Part 02
+#Poisson Distribution
+#Random variable X has poisson distribution with lamda = 12
+
+#Part 03
+#P(X = 15)
+dpois(15,12)
